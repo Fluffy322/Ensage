fcee0569e44944f8524c824d6829d90a19af1bfe
Fluffy322 <Flavius.Smantanica@gmx.de>
2016-11-03 18:58:45 +0100

    a

--- NecroSharp/NecroSharp/Program.cs
+++ NecroSharp/NecroSharp/Program.cs
@@ -10,7 +10,7 @@ namespace NecroSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World2");
         }
     }
 }
