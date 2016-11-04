7639eb3a717b3962bd758c855d5143c6311e7309
Fluffy322 <Flavius.Smantanica@gmx.de>
2016-11-05 00:15:13 +0100

    a

--- BountyHunter AutoTrack/BountyHunter AutoTrack/Program.cs
+++ BountyHunter AutoTrack/BountyHunter AutoTrack/Program.cs
@@ -6,33 +6,75 @@ using System.Threading.Tasks;
 using Ensage;
 using Ensage.Common;
 using SharpDX;
+using Ensage.Common.Menu;
 
 namespace BountyHunter_AutoTrack
 {
+    
+
     class Program
     {
+        private static readonly Menu Menu = new Menu("AutoTrack", "autotrack", true, "npc_dota_hero_bounty_hunter", true);
+        private static bool IsStealthed = false;
+
         static void Main(string[] args)
         {
-            Game.PrintMessage("AutoTrack v 0.0 loaded -> only you can see this message.", 0);
+            Game.PrintMessage("AutoTrack v 1.0 loaded", 0);
             Game.OnUpdate += Game_OnUpdate;
+
+ 
+
+            Menu.AddToMainMenu();
+
+            Menu.AddItem(new MenuItem("AutoTrack", "Enable Autotrack").SetValue(true));
+            Menu.AddItem(new MenuItem("hpthreshold", "HP Threshold").SetValue(new Slider(100, 1)).SetTooltip("Using Track on Targets with HP lower than x%"));
+
+            var interruptMenu = new Menu("Interrupt Stealth", "interruptMenu", false);
+            interruptMenu.AddItem(new MenuItem("dontCancelStealth", "Dont cancel Stealth?").SetValue(true));
+            interruptMenu.AddItem(new MenuItem("onlyIfReady", "Only cancel if Stealth is ready").SetValue(true));
+            Menu.AddSubMenu(interruptMenu);
         }
 
         private static void Game_OnUpdate(EventArgs args)
         {
             var me = ObjectMgr.LocalHero;
+
+            if (me.Modifiers.FirstOrDefault(e => e.Name == "modifier_bounty_hunter_wind_walk") != null)
+                IsStealthed = true;
+            else
+                IsStealthed = false;
+                
+            if (me == null || !me.IsAlive || !Menu.Item("AutoTrack").GetValue<bool>())
+                return;
+            if (IsStealthed && Menu.Item("dontCancelStealth").GetValue<bool>())
+                return;
+            if (IsStealthed && Menu.Item("onlyIfReady").GetValue<bool>() && me.Spellbook.Spell3.Cooldown > 0)
+                return;
+
+            
+
             var rSpell = me.Spellbook.Spell4;
 
-            uint healthtreshhold = 50;
+            int healthtreshhold = Menu.Item("hpthreshold").GetValue<Slider>().Value;
 
 
             if (me.Mana > me.Spellbook.Spell4.ManaCost && rSpell.Cooldown == 0 && me.IsAlive)
             {
-                //no double track einfügen, invis abfrage einfügen, sleep einfügen
-                var enemies = ObjectMgr.GetEntities<Hero>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsIllusion && Range(me, x) < rSpell.CastRange * rSpell.CastRange && x.Health * 100 / x.MaximumHealth < healthtreshhold);
+                //to-Do, lowest life first
                 
-                if (enemies != null)
+                var enemies = ObjectMgr.GetEntities<Hero>().Where(x => x.Team != me.Team && x.IsAlive && !x.IsIllusion && Range(me, x) < rSpell.CastRange * rSpell.CastRange && x.Health * 100 / x.MaximumHealth <= healthtreshhold && Utils.SleepCheck(me.Handle + "track") && x.Modifiers.FirstOrDefault(e => e.Name == "modifier_bounty_hunter_track") == null).ToList();
+
+                if (enemies.Count() > 0)
                 {
-                    rSpell.UseAbility(enemies.First());
+                    rSpell.UseAbility(enemies[0]);
+                    Utils.Sleep(700, me.Handle + "track");
+                    Game.PrintMessage(enemies[0].Name + " tracked", 0);
+                    foreach (var item in enemies[0].Modifiers)
+                    {
+                        Game.PrintMessage(item.Name, 0);
+                    }
+
+
                 }
             }
 
